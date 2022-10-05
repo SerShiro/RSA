ec81d2534496cae9b8db58e5ad386b3e4a0228e6
SerShiro <seregapolpro@gmail.com>
2022-10-05 14:00:13 +0300

    create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# RSA
+
+Encrypt and Decrypt RSA cipher 
---
+++ script.js
@@ -0,0 +1,37 @@
+const inputText = document.querySelector('#input')
+const inputDec = document.querySelector('#inputDec')
+const btnEnc = document.querySelector('#btnEnc')
+const btnDec = document.querySelector('#btnDec')
+
+
+const PQKeys = () => {
+    for (let i = 0; i < 1000; i++) {
+        const element = array[i];
+        
+    }
+}
+
+function encrypt() {
+    
+}
+
+function decrypt() {
+    
+}
+
+inputText.addEventListener('keypress', event => {
+    
+})
+
+btnEnc.addEventListener('click', (event) => {
+    
+})
+
+btnDec.addEventListener('click', (event) => {
+    
+})
+
+inputDec.addEventListener('keypress', event => {
+    
+    
+})
